d1dc0ec4dd8bfd4b0570d0ff08f3c734ae161762
shiftpop0 <w1095406800@163.com>
2023-11-03 18:49:32 +0800

    添加：根据最后一次打分

--- Zhujiao_SJJG/configuration.py
+++ Zhujiao_SJJG/configuration.py
@@ -2,6 +2,15 @@ import os
 import re
 
 
+# def has_letter(input_string: str):  # 判断是否有字母在字符串中
+#     # 使用正则表达式来匹配字母
+#     pattern = r'[a-zA-Z]'
+#     if re.search(pattern, input_string):
+#         return True
+#     else:
+#         return False
+
+
 def find_experiment_file(directory, n):
     # 构建正则表达式以匹配文件名中包含"实验n"的部分
     pattern = re.compile(fr'.*实验{n}.*')
@@ -15,18 +24,13 @@ def find_experiment_file(directory, n):
     return matching_files
 
 
-# 指定目录和n的值
-directory = '/path/to/your/directory'  # 将目录路径替换为你的目录路径
-n = 1  # 替换为你要查找的n的值
-matching_files = find_experiment_file(directory, n)
-if matching_files:
-    print("找到匹配的文件：")
-    for filename in matching_files:
-        print(filename)
-else:
-    print("未找到匹配的文件。")
-
-
 def re_file_list(n: int) -> dict['str':'str']:
     directory = './EXCEL/'  # 将目录路径替换为你的目录路径
     n = 1  # 替换为你要查找的n的值
+    matching_files = find_experiment_file(directory, n)
+    if matching_files:
+        print("找到匹配的文件：")
+        for filename in matching_files:
+            print(filename)
+    else:
+        print("未找到匹配的文件。")
--- Zhujiao_SJJG/main.py
+++ Zhujiao_SJJG/main.py
@@ -1,28 +1,38 @@
 import requests
 import pandas as pd
 import json
-from datetime import datetime
+from datetime import datetime, timedelta
+import configuration as cf
 
 
+# def time_to_seconds(h, m, s=0):
+#     """将时、分、秒转换为总秒数"""
+#     return h * 3600 + m * 60 + s
+#
+#
+# def seconds_to_time(total_seconds):
+#     """将总秒数转换为天、小时、分钟、秒的格式"""
+#     days, total_seconds = divmod(total_seconds, 24 * 3600)
+#     hours, total_seconds = divmod(total_seconds, 3600)
+#     minutes, seconds = divmod(total_seconds, 60)
+#     return days, hours, minutes, seconds
 
-def time_to_seconds(h, m, s=0):
-    """将时、分、秒转换为总秒数"""
-    return h * 3600 + m * 60 + s
-
-
-def seconds_to_time(total_seconds):
-    """将总秒数转换为天、小时、分钟、秒的格式"""
-    days, total_seconds = divmod(total_seconds, 24 * 3600)
-    hours, total_seconds = divmod(total_seconds, 3600)
-    minutes, seconds = divmod(total_seconds, 60)
-    return days, hours, minutes, seconds
-
+def days_diff(begin_time:str,time2:str)->int:
+    time_obj1 = datetime.fromisoformat(begin_time)
+    time_obj2 = datetime.fromisoformat(time2)
+    first_day = time_obj1.day
+    second_day = (time_obj1 + timedelta(days=1)).day
+    if time_obj2.day == first_day:
+        return 0
+    elif time_obj2.day == second_day:
+        return 1
+    else:
+        return -1
 
 # s1 = "2023-09-04T14:10:00+08:00"
 def time_difference(s1, s2) -> bool:
-    time_format = "%Y-%m-%dT%H:%M:%S.%f%z"
-    time_obj1 = datetime.strptime(s1, time_format)
-    time_obj2 = datetime.strptime(s2, time_format)
+    time_obj1 = datetime.fromisoformat(s1)
+    time_obj2 = datetime.fromisoformat(s2)
     return time_obj1 > time_obj2
 
 
@@ -48,14 +58,7 @@ class Stu(object):
             "score_value_5": [25, 25, 25, 25],
             "score_value_6": [20, 20, 20, 20, 20]
         }
-        self.file = {
-            "file1": './EXCEL/测验149 【金融科技】【不计分】实验1——顺序表-编程排行榜2023_10_19 01_09_19.xlsx',
-            "file2": './EXCEL/测验169 【金融科技】【计分】实验2——链表-编程排行榜2023_10_19 01_10_11.xlsx',
-            "file3": './EXCEL/测验185 【金融科技】【计分】实验3——栈-编程排行榜2023_10_19 01_10_44.xlsx',
-            "file4": './EXCEL/测验213 【金融科技】【计分】实验4——栈补充&队列-编程排行榜2023_10_19 00_51_28.xlsx',
-            "file5": './EXCEL/测验223 【金融科技】【计分】实验5——串-编程排行榜2023_10_18 22_27_20.xlsx',
-            "file6": './EXCEL/测验239 【金融科技】【计分】实验6——串补充&树-编程排行榜2023_11_2 12_54_13.xlsx'
-        }
+
         self.exp_id = {
             '实验1': 149,
             '实验2': 169,
@@ -65,34 +68,57 @@ class Stu(object):
             '实验6': 239
         }
         self.n = n
-        self.df = pd.read_excel(self.file['file{}'.format(n)], dtype={'用户名': str})
         self.df_cz = pd.read_excel('./EXCEL/查重{}.xlsx'.format(n), dtype={'用户A': str, '用户B': str})
-        self.start_time = time_to_seconds(14, 10)  # 开始时间 14：10
+        # self.start_time = time_to_seconds(14, 10)  # 开始时间 14：10
         self.copy_dict: dict[str, list[str]] = {}  # 抄袭人名单
         self.copy_pro_dict: dict[str, list[str]] = {}  # 被抄袭名单
-        self.pre_process()  # 预处理：删除助教和老师
+        self.problem_list = self.pre_process()  # 预处理：删除助教和老师
         self.cha_chong()
+        self.stu_dict, self.stu_list, self.group_id = self.get_stu_list()
+        self.exp_begin_time=self.get_exp_begin_time()
+
+    def get_exp_begin_time(self):
+        # limit = 10 默认
+        url = "http://172.31.221.67/api/contests/?offset=0&limit=100&group={}".format(self.group_id)
+        response = requests.get(url, headers=self.headers)
+        exp_list = json.loads(response.text)['results']
+        exp_dict = {}
+        for index,item in enumerate(exp_list):
+            exp_dict[item.pop('id')]=item
+        return exp_dict
+
+    def get_stu_list(self) -> (dict,list,int): #获取人员名单
+        #先获取班级group id
+        url = "http://172.31.221.67/api/manage/groups/?limit=10"
+        response = requests.get(url, headers=self.headers)
+        group_id = json.loads(response.text)['results'][0]['id']
+        url = "http://172.31.221.67/api/groups/{}/".format(group_id)
+        response = requests.get(url, headers=self.headers)
+        stu_user:list = json.loads(response.text)['users']
+        sorted_list = sorted(stu_user, key=lambda x: int(x['username'])) # 按学号升序
+        final_dict: dict[str: dict] = {}
+        final_list: list[int] = []
+        for item in sorted_list:
+            final_list.append(item['username'])
+            final_dict[item.pop('username')] = item # {username :{id,nickname,name}}
+        return final_dict, final_list, group_id
+
 
     def pre_process(self):
-        # 删除老师和助教
-        rows_to_delete = []
-        for index, row in self.df.iterrows():
-            if 'szu' in row['用户名'] or 'zhujiao' in row['用户名']:
-                rows_to_delete.append(index)
-        self.df = self.df.drop(index=rows_to_delete)
-        self.df = self.df.reset_index(drop=True)
-
-        rows_to_delete_cz = []
-        for index, row in self.df_cz.iterrows():
-            if 'szu' in str(row['用户A']) or 'szu' in str(row['用户B']):
-                rows_to_delete_cz.append(index)
-        self.df_cz = self.df_cz.drop(index=rows_to_delete_cz)
-        self.df_cz = self.df_cz.reset_index(drop=True)
-
-    def cha_chong(self):
         # 先获取题目id列表
         url = 'http://172.31.221.67/api/contests/problems_id/?contest={}'.format(self.exp_id['实验{}'.format(self.n)])
         problem_list: list = json.loads(requests.get(url, headers=self.headers).text)["problems_id"]["0"]
+
+        # 删除老师和助教
+        rows_to_delete_cz = []
+        for index, row in self.df_cz.iterrows():
+            if any(substring in str(row['用户A']) + str(row['用户B']) for substring in ['szu', 'zhujiao']):
+                rows_to_delete_cz.append(index)
+        self.df_cz = self.df_cz.drop(index=rows_to_delete_cz)
+        self.df_cz = self.df_cz.reset_index(drop=True)
+        return problem_list
+
+    def cha_chong(self):
 
         # 再查表格
         for index, row in self.df_cz.iterrows():
@@ -109,7 +135,7 @@ class Stu(object):
                 user_copy_pro = sub_a
             # if 'szu' in str(user_copy['user']['username']) or 'szu' in str(user_copy_pro['user']['username']):
             #     continue
-            problem_word: str = chr(problem_list.index(row['问题ID']) + 65)  # 将下标转化成字母ABCD
+            problem_word: str = chr(self.problem_list.index(row['问题ID']) + 65)  # 将下标转化成字母ABCD
             # 抄袭成功：
             if user_copy['status'] == 0:
                 # 记录抄袭者
@@ -120,7 +146,7 @@ class Stu(object):
                     self.copy_dict[user_copy['user']['username']] = [problem_word]
 
                 # 记录被抄袭者
-                if row['问题ID'] == 167:  # 167问题特判
+                if row['问题ID'] == 167:  # 167问题特判 抄和被抄都记到抄的名单copy_dict
                     if self.copy_dict.get(user_copy_pro['user']['username']):
                         if problem_word not in self.copy_dict[user_copy_pro['user']['username']]:
                             self.copy_dict[user_copy_pro['user']['username']].append(problem_word)
@@ -135,64 +161,75 @@ class Stu(object):
 
     # 记录新分数
     def get_new_record(self):
-        col = self.df.columns.values[5:]
-        list_score = [[] for _ in range(len(col) + 2)]  # 多一行总分与说明
+        file_data = {'用户名': [], '姓名': []}
+        for i in range(len(self.problem_list)):
+            key = '题{}'.format(chr(i + 65))  # 将下标转化成字母ABCD
+            file_data[key] = []
+        # 加入所有AC记录
+        url = "http://172.31.221.67/api/code-submission/?offset=0&limit=100&contest={}&problem=&search=&status=".format(self.exp_id['实验{}'.format(self.n)])
+        response = json.loads(requests.get(url, headers=self.headers).text)
+        sub_AC_count = int(response['count'])
+        sub_AC_results = list(response['results'])
+        tmp_num = 100
+        while tmp_num < sub_AC_count:
+            url = "http://172.31.221.67/api/code-submission/?offset={}&limit=100&contest={}&problem=&search=&status=".format(tmp_num, self.exp_id['实验{}'.format(self.n)])
+            response = json.loads(requests.get(url, headers=self.headers).text)
+            sub_AC_results.extend(list(response['results']))
+            tmp_num += 100
 
-        for index, row in self.df.iterrows():
-
-            caption = ''  # 修正分数原因
-            # 按提交时间给分数打折
-            for i, problem_order in enumerate(col):
-                if str(row[problem_order]) == 'nan':
-                    list_score[i].append(0)
-                    continue
-                time = str(row[problem_order]).split('(')[0]
-
-                if time != '':
-                    h, m, s = map(int, time.split(":"))
-                    total_seconds = self.start_time + time_to_seconds(h, m, s)
-                    days, hours, minutes, seconds = seconds_to_time(total_seconds)
-                    if days == 1:
-                        discount = 0.8
-                    elif days > 1:
-                        discount = 0.6
-                    else:
+        # list_score = [[] for _ in range(len(self.problem_list) + 2)]  # 多一行总分与说明p
+        user_dict: dict[tuple, dict] = {}  # （ (用户名, 题) ：item）
+        for index, item in enumerate(sub_AC_results):
+            if any(substring in item['user']['username'] for substring in ['szu', 'zhujiao']):
+                continue
+            tmp_tuple = (item['user']['username'], item['problem'])
+            if (not user_dict.get(tmp_tuple)) or (not time_difference(user_dict[tmp_tuple]['time'], item['time'])):  # 保留时间更晚的记录
+                user_dict[tmp_tuple] = item
+        caption_list = []  # 修正分数原因
+        total_score_list = []
+        for index,xuehao in enumerate(self.stu_list):
+            file_data['用户名'].append(xuehao)
+            file_data['姓名'].append(self.stu_dict[xuehao]['name'])
+            for i in range(len(self.problem_list)):
+                problem = '题{}'.format(chr(i + 65))
+                key=(xuehao,self.problem_list[i])
+                if user_dict.get(key):
+                    days = days_diff(self.exp_begin_time[self.exp_id['实验{}'.format(self.n)]]['begin_time'],user_dict[key]['time'])
+                    if days == 0:
                         discount = 1.0
+                    elif days ==1:
+                        discount = 0.8
+                    else:
+                        discount = 0.6
                 else:
-                    discount = 0
+                    discount=0
                 score = self.score_value_proto['score_value_{}'.format(self.n)][i] * discount
-                list_score[i].append(score)
-            # 计算总分
-            total_score = sum(lst[index] for lst in list_score[:len(col)])
+                file_data[problem].append(score if user_dict.get(key) and user_dict[key]['status']==0 else 0)
+            caption = ''
+            total_score = sum(lst[index] for lst in list(file_data.values())[-len(self.problem_list):])
 
             # 找出抄袭题
-            if self.copy_dict.get(row['用户名']):
+            if self.copy_dict.get(xuehao):
                 total_score = min(60, total_score)
-            list_score[len(col)].append(total_score)
+            total_score_list.append(total_score)
             # 说明原因
-            if self.copy_dict.get(row['用户名']):
+            if self.copy_dict.get(xuehao):
                 caption += '抄袭题：'
-                for item in self.copy_dict[row['用户名']]:
+                for item in self.copy_dict[xuehao]:
                     caption += (item + ' ')
-            if self.copy_pro_dict.get(row['用户名']):
+            if self.copy_pro_dict.get(xuehao):
                 caption += '被抄袭题：' if len(caption) == 0 else ', 被抄袭题：'
-                for item in self.copy_pro_dict[row['用户名']]:
+                for item in self.copy_pro_dict[xuehao]:
                     caption += (item + ' ')
-            list_score[-1].append(caption)
-
-        for index, item in enumerate(list_score[:len(col)]):
-            self.df.insert(len(self.df.columns), str(col[index]) + '修正分', item)
-        self.df.insert(len(self.df.columns), '修正总分', list_score[-2])
-        self.df.insert(len(self.df.columns), '说明（首日提交全额给分，次日8折，第三天6折；如有任意抄袭，总分将不高于60；\n被抄袭者获得警告，被抄袭者也可能是抄袭网上的）', list_score[-1])
-
-        # 去掉无用列 并写入excel
-        cols_to_drop = self.df.columns[4:5 + len(col)]
-        self.df = self.df.drop(columns=cols_to_drop)
-        self.df.to_excel('./EXCEL/上机得分{}.xlsx'.format(self.n), index=False)
+            caption_list.append(caption)
+        file_data['修正总分'] = total_score_list
+        file_data['说明（首日提交全额给分，次日8折，第三天6折；如有任意抄袭，总分将不高于60；\n被抄袭者获得警告，被抄袭者也可能是抄袭网上的）'] = caption_list
+        df = pd.DataFrame(file_data)
+        df.to_excel('./EXCEL/new上机得分{}.xlsx'.format(self.n), index=False)
 
 
 def main():
-    stu = Stu(2)
+    stu = Stu(6)
     stu.get_new_record()
 
 
